a3716b4591b5dd8c5af4f9440cd05b4533f565b1
Rintt <ufukalgac@hotmail.com>
2022-02-15 16:27:50 +0100

    added filterService 'cmyservice'

---
+++ src/cmyservice/client.js
@@ -0,0 +1,61 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+require('@google-cloud/trace-agent').start();
+
+const path = require('path');
+const grpc = require('grpc');
+const leftPad = require('left-pad');
+const pino = require('pino');
+
+const PROTO_PATH = path.join(__dirname, './proto/demo.proto');
+const PORT = 5000;
+
+const shopProto = grpc.load(PROTO_PATH).hipstershop;
+const client = new shopProto.FilterService(`localhost:${PORT}`,
+  grpc.credentials.createInsecure());
+
+const logger = pino({
+  name: 'filterservice-client',
+  messageKey: 'message',
+  changeLevelName: 'severity',
+  useLevelLabels: true
+});
+
+const request = {
+    filter_code: 'kitchen',
+};
+
+function _filterCatalog (m) {
+  return `${m.units}.${m.nanos.toString().padStart(9,'0')} ${m.filter_code}`;
+}
+
+client.getSupportedCategories({}, (err, response) => {
+  if (err) {
+    logger.error(`Error in getSupportedCategories: ${err}`);
+  } else {
+    logger.info(`filter codes: ${response.filter_codes}`);
+  }
+});
+
+client.filter(request, (err, response) => {
+  if (err) {
+    logger.error(`Error in filter: ${err}`);
+  } else {
+    logger.log(`Filter: ${request} res ${_filterCatalog(response)}`);
+  }
+});
---
+++ src/cmyservice/server.js
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// if(process.env.DISABLE_PROFILER) {
+//   console.log("Profiler disabled.")
+// }
+// else {
+//   console.log("Profiler enabled.")
+//   require('@google-cloud/profiler').start({
+//     serviceContext: {
+//       service: 'filterservice',
+//       version: '1.0.0'
+//     }
+//   });
+// }
+
+
+// if(process.env.DISABLE_TRACING) {
+//   console.log("Tracing disabled.")
+// }
+// else {
+//   console.log("Tracing enabled.")
+//   require('@google-cloud/trace-agent').start();
+// }
+
+// if(process.env.DISABLE_DEBUGGER) {
+//   console.log("Debugger disabled.")
+// }
+// else {
+//   console.log("Debugger enabled.")
+//   require('@google-cloud/debug-agent').start({
+//     serviceContext: {
+//       service: 'filterservice',
+//       version: 'VERSION'
+//     }
+//   });
+// }
+
+const path = require('path');
+const grpc = require('@grpc/grpc-js');
+const pino = require('pino');
+const protoLoader = require('@grpc/proto-loader');
+
+const MAIN_PROTO_PATH = path.join(__dirname, './proto/demo.proto');
+const HEALTH_PROTO_PATH = path.join(__dirname, './proto/grpc/health/v1/health.proto');
+
+const PORT = 5000;
+
+const shopProto = _loadProto(MAIN_PROTO_PATH).hipstershop;
+const healthProto = _loadProto(HEALTH_PROTO_PATH).grpc.health.v1;
+
+const logger = pino({
+  name: 'filterservice-server',
+  messageKey: 'message',
+  changeLevelName: 'severity',
+  useLevelLabels: true
+});
+
+/**
+ * Helper function that loads a protobuf file.
+ */
+function _loadProto (path) {
+  const packageDefinition = protoLoader.loadSync(
+    path,
+    {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true
+    }
+  );
+  return grpc.loadPackageDefinition(packageDefinition);
+}
+
+/**
+ * Helper function that gets currency data from a stored JSON file
+ * Uses public data from European Central Bank
+ */
+function _getProducts (callback) {
+  const data = require('./data/product_catalog.json');
+  callback(data);
+}
+
+/**
+ * Lists the supported currencies
+ */
+function getSupportedCategories (call, callback) {
+  logger.info('Getting supported currencies...');
+  _getProducts((data) => {
+    callback(null, {categories: Object.keys(data.categories)});
+  });
+}
+
+/**
+ * Filters the products according to the 'from' in request
+ */
+function filter (call, callback) {
+  try {
+    _getProducts((data) => {
+      const request = call.request;
+      // Convert: from_currency --> EUR
+      const from = request.from;
+      from.filter_code
+      results = data
+      results.filter(ele => {
+        ele.categories.includes(from.filter_code) ? true : false
+      })
+      logger.info(`filter request successful`);
+      callback(null, result);
+    });
+  } catch (err) {
+    logger.error(`filter request failed: ${err}`);
+    callback(err.message);
+  }
+}
+
+/**
+ * Endpoint for health checks
+ */
+function check (call, callback) {
+  callback(null, { status: 'SERVING' });
+}
+/**
+ * Starts an RPC server that receives requests for the
+ * CurrencyConverter service at the sample server port
+ */
+function main () {
+  logger.info(`Starting gRPC server on port ${PORT}...`);
+  const server = new grpc.Server();
+  server.addService(shopProto.FilterService.service, {getSupportedCategories, filter});
+    // , convert});
+  server.addService(healthProto.Health.service, {check});
+
+  server.bindAsync(
+    `0.0.0.0:${PORT}`,
+    grpc.ServerCredentials.createInsecure(),
+    function() {
+      logger.info(`FilterService gRPC server started on port ${PORT}`);
+      server.start();
+    },
+   );
+}
+
+main();
